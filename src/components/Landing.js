33e32f30ce7eeaaf3fc40ea0f49cc9a6b1318814
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-08 23:58:45 +0330

    feat: added search functinality for crypto coins

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,17 +6,25 @@ import Coin from './Coin';
 import Loader from './Loader';
 
 const Landing = () => {
+      //States
       const [coins, setCoins] = useState([]);
+      const [search, setSearch] = useState('');
       useEffect(() => {
             const fetchAPI = async () => {
                   setCoins(await getCoin());
             };
             fetchAPI();
       }, []);
+      //Handling search
+      const searchOnChange = e => {
+            setSearch(e.target.value);
+      };
+      const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
       return (
             <>
-                  <input type='text' placeholder='Search' />
-                  {coins.length ? coins.map(coin => <Coin key={coin.id} coin={coin} />) : <Loader />}
+                  <input type='text' placeholder='Search' value={search} onChange={searchOnChange} />
+                  {coins.length ? searchedCoins.map(coin => <Coin key={coin.id} coin={coin} />) : <Loader />}
             </>
       );
 };
